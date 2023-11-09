bf900776a137bc01cee470e9da10f0c06a076b7c
Mittaanjali <mitta.anjali@mphasis.com>
2023-11-09 14:45:30 +0530

    variables and datatypes

---
+++ variables and datatypes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables_and_datatypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string name = "Your Name";
+            int age = 20; 
+            double height = 4.5; 
+
+            
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Age: " + age + " years");
+            Console.WriteLine("Height: " + height + " feet");
+
+            Console.ReadLine();
+        }
+    }
+}
+    
+
